5620f840c92b4b9889269828097906b5e0af5cf7
TharunVirupakshi <tharunvrrvc184@gmail.com>
2024-02-28 20:11:44 +0530

    Downgraded React to solve dep issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import { ApiDataProvider } from './context/dataProvider.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <ApiDataProvider>
       <App />
     </ApiDataProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
